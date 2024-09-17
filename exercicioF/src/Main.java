c2e00dde748d84e7109c530e835f8499787200f3
Skall <nicolasrodriguesemery@gmail.com>
2024-09-17 15:41:06 -0300

    novo2.0

--- exercicioF/src/Main.java
+++ exercicioF/src/Main.java
@@ -1,26 +1,57 @@
 import java.util.Scanner;
 
-public class Main
-{
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Scanner sc2 = new Scanner(System.in);
+        Scanner sc3 = new Scanner(System.in);
         Aluno a = new Aluno();
+        Professor p = new Professor();
+        int r;
 
 
-        System.out.println("Nos informe o nome do aluno");
-        a.setNome(sc.nextLine());
+        do {
+            System.out.println("Aluno, digite: [1], professor, digite: [2]");
+            int x = sc2.nextInt();
+            switch (x) {
+                case 1:
 
-        System.out.println("Primeira nota?");
-        a.setNota1(sc.nextDouble());
+                    System.out.println("Nos informe o nome do aluno");
+                    a.setNome(sc3.nextLine());
 
-        System.out.println("Segunda nota?");
-        a.setNota2(sc.nextDouble());
+                    System.out.println("Primeira nota?");
+                    a.setNota1(sc.nextDouble());
 
-        System.out.println("Terceira nota?");
-        a.setNota3(sc.nextDouble());
+                    System.out.println("Segunda nota?");
+                    a.setNota2(sc.nextDouble());
 
-        a.calc_media();
-        a.detalhes();
+                    System.out.println("Terceira nota?");
+                    a.setNota3(sc.nextDouble());
 
+                    a.calc_media();
+                    a.detalhes();
+                    break;
+
+                case 2:
+
+
+                    System.out.println("Nome do professor");
+                    p.setNome(sc.nextLine());
+
+                    System.out.println("Qual a carga horaria?");
+                    p.setHora_trabalhada(sc2.nextInt());
+
+                    System.out.println("Valor da hora trabalhada?");
+                    p.setValor_hora(sc2.nextDouble());
+
+                    p.calc_salario();
+                    p.Detalhes();
+
+
+
+            }
+            System.out.println("Para repetir digite [0]");
+            r = sc.nextInt();
+        }while (r == 0);
     }
 }
--- exercicioF/src/Professor.java
+++ exercicioF/src/Professor.java
@@ -1,2 +1,38 @@
-public class Professor {
+public class Professor extends Pessoa {
+
+    private double Valor_hora, salario;
+    private int hora_trabalhada;
+
+    public double getValor_hora() {
+        return Valor_hora;
+    }
+
+    public void setValor_hora(double valor_hora) {
+        Valor_hora = valor_hora;
+    }
+
+    public int getHora_trabalhada() {
+        return hora_trabalhada;
+    }
+
+    public void setHora_trabalhada(int hora_trabalhada) {
+        this.hora_trabalhada = hora_trabalhada;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public double calc_salario(){
+        salario = hora_trabalhada * Valor_hora;
+        return salario;
+    }
+
+    public void Detalhes(){
+
+        System.out.println("Professor(a): "+ getNome());
+        System.out.println("Hora trabalhada: "+ getHora_trabalhada());
+        System.out.println("Valor da hora: "+ getValor_hora());
+        System.out.println("Salário: "+ calc_salario());
+    }
 }
