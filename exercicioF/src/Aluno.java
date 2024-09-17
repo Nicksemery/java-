7cf317071269d2ab815e3c469f14dc5ca5752c2c
Skall <nicolasrodriguesemery@gmail.com>
2024-09-17 15:04:33 -0300

    novo2.0

--- exercicioF/src/Aluno.java
+++ exercicioF/src/Aluno.java
@@ -1,16 +1,7 @@
-public class Aluno {
-
-    private String nome ;
+public class Aluno extends Pessoa {
 
     private double nota1 , nota2, nota3, media;
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 
     public double getNota1() {
         return nota1;
@@ -56,5 +47,14 @@ public class Aluno {
         } else{
            return ("Reprovado");
         }
+    }
+
+    public void detalhes(){
+
+        System.out.println("situação do aluno.");
+        System.out.println("Média do aluno: " + getNome());
+        System.out.println(getMedia());
+        System.out.println("Quadro de notas " + getNota1()+ "/" + getNota2()+ "/" + getNota3());
+        System.out.println("Situação:" + situacao());
     }
 }
--- exercicioF/src/Main.java
+++ exercicioF/src/Main.java
@@ -6,6 +6,7 @@ public class Main
         Scanner sc = new Scanner(System.in);
         Aluno a = new Aluno();
 
+
         System.out.println("Nos informe o nome do aluno");
         a.setNome(sc.nextLine());
 
@@ -19,12 +20,7 @@ public class Main
         a.setNota3(sc.nextDouble());
 
         a.calc_media();
-
-        System.out.println("situação do aluno.");
-        System.out.println("Média do aluno: " + a.getNome());
-        System.out.println(a.getMedia());
-        System.out.println("Quadro de notas " + a.getNota1()+ "/" + a.getNota2()+ "/" + a.getNota3());
-        System.out.println("Situação:" + a.situacao());
+        a.detalhes();
 
     }
 }
